a7aa315bde5b3c8e79000d3641d30d7976f4ab98
siamofuori <pepito>
2016-04-19 19:04:46 -0300

    Commit inicial de frontend

---
+++ desapp-groupd-frontend/app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name desappGroupdFrontendApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the desappGroupdFrontendApp
+ */
+angular.module('desappGroupdFrontendApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
